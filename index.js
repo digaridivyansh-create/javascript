31265636b5c098af2cbf7d40ccb686984990c407
digaridivyansh-create <digaridivyansh@gmail.com>
2026-02-21 18:15:50 +0530

    Add

---
+++ index.js
@@ -0,0 +1,23 @@
+{
+ let a=10;
+ console.log(a);
+}
+let price=100;
+let x=null;
+let y=BigInt(123);
+let z=Symbol("hello");
+const student={
+    name:"rahul",
+    age:20,
+    cgpa:8.2,
+    ispass:true
+};
+student["age"]=student["age"]+1;
+console.log(student["age"]);
+const product={
+    title:"ball pen",
+    rating:4,
+    offer:5,
+    price:270
+};
+console.log(product);
