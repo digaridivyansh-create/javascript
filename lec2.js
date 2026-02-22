cac757477297f0242dcf63d82a70c8381e9752e2
digaridivyansh-create <digaridivyansh@gmail.com>
2026-02-22 18:06:39 +0530

    add

---
+++ lec2.js
@@ -0,0 +1,117 @@
+//artirhmetic oprators
+//let a = 10;
+//let b = 20;
+//console.log(a + b);
+
+//let c=44;
+//let d=5;
+//console.log("c+d=",c+d);
+//console.log("c-d=",c-d);
+//console.log("c*d=",c*d);
+//console.log("c/d=",c/d);
+//console.log("c%d=",c%d);
+//console.log("c**d=",c**d);
+
+//let a=5;
+//let b=2;
+//console.log("a=",a,"& b=",b);
+//a=a+1;
+//console.log("a=",a);
+//a--;
+//console.log("a=",a);
+
+//let a=10;
+//let b=2;
+
+//console.log("a=",a,"& b=",b);
+//console.log(++a);
+//--b;
+//console.log(b);
+
+//assignment oprators
+//let a=10;
+//let b=4;
+//console.log("a=",a,"& b=",b);
+//a+=4;
+//console.log("a=",a);
+//a*=5;
+//console.log("a=",a);
+//a%=b;
+//console.log("a=",a);
+//a-=5;
+//console.log("a=",a);
+
+//let color;
+//let mode="dark";
+//if(mode=="dark"){
+    //color="black";
+//}
+/*else{
+    color="white";
+}
+console.log(color);*/
+
+/*let num=10;
+if(num%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}*/
+
+/*let mode="dark";
+let color;
+if(mode==="dark"){
+    color="black";
+}
+else if(mode==="blue"){
+    color="blue";
+}
+else if(mode==="pink"){
+    color="pink";
+}
+else{
+    color="white";
+}
+console.log(color);
+
+if(mode==="dark"){
+    console.log(mode);
+}*/
+
+/*let age=35;
+let result=age>=18?"adult":"child";
+console.log(result);*/
+
+
+/*let name=prompt("hello");
+console.log(name);*/
+
+/*let num=prompt("enter a number");
+if(num%5==0){
+    console.log(num,"is divisible by 5");
+}
+else{
+    console.log(num,"is not divisible by 5");
+}*/
+let score=prompt("enter your score(0,100):");
+let grade;
+if(score>=90 && score<=100){
+    grade="A";
+}
+else if(score>=70 && score<=89){
+    grade="B";
+}
+else if(score>=60 && score<=69){
+    grade="C";
+}
+else if(score>=50 && score<=59){
+    grade="D";
+}
+else if(score>=0 && score<=49){
+    grade="F";
+}
+console.log("according to youe score your grade is",grade);
+
+
+
