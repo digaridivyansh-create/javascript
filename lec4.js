927e679336f517a2f9e6012e87ddee8fdac15f42
digaridivyansh-create <digaridivyansh@gmail.com>
2026-03-02 17:16:37 +0530

    add

---
+++ lec4.js
@@ -0,0 +1,67 @@
+/*let marks=[87,45,6,3,68];
+console.log(marks[0]);
+console.log(marks[1]);
+console.log(marks[2]);
+console.log(marks[3]);
+console.log(marks[4]);
+console.log(marks.length);*/
+
+/*let str=["hello","ironman","thor","hulk"];
+for(let idx=0;idx<str.length;idx++){
+    console.log(str[idx]);
+}*/
+
+/*let cities=["delhi","mumbai","kolkata","chennai"];
+for(let city of cities){
+    console.log(city.toUpperCase());
+}*/
+
+/*let marks=[85,97,44,37,76,60];
+let average=0;
+for(let i=0;i<marks.length;i++){
+    average+=marks[i];
+    
+}
+average=average/marks.length;
+console.log("average marks is=",average);*/
+
+/*let items=[250,645,300,900,50];
+for(let i=0;i<items.length;i++){
+    let offer=items[i]/10;
+    items[i]-=offer;
+}
+console.log(items);*/
+
+/*let food=["pizza","burger","pasta","sushi"];
+food.push("ice cream");
+console.log(food);
+food.pop();
+console.log(food);*/
+
+/*let food=["pizza","burger","pasta","sushi"];
+console.log(food.toString())
+console.log(food);*/
+
+/*let food=["pizza","burger","pasta","sushi"];
+let hero=["ironman","thor","hulk","captain america"];
+let combined=food.concat(hero);
+console.log(combined);*/
+
+/*let heros=["ironman","thor","hulk","captain america"];
+heros.unshift("black widow");
+console.log(heros);*/
+
+/*let heros=["ironman","thor","hulk","captain america"];
+let val=heros.shift();
+console.log(val);*/
+
+/*let heros=["ironman","thor","hulk","captain america"];
+console.log(heros.slice(1,3));*/
+
+let companies=["Google","Netflix","Microsoft","Uber","Bloomberg","IBM"];
+console.log(companies.shift());
+console.log(companies);
+console.log(companies.push("Amazon"));
+console.log(companies);
+
+
