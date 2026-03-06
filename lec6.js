62ddceb895a933a76a80dbc7f943b3e7829be08a
digaridivyansh-create <digaridivyansh@gmail.com>
2026-03-06 18:10:06 +0530

    add

---
+++ lec6.js
@@ -0,0 +1,38 @@
+/*console.log("hello world");
+window.console.log("hello world");*/
+
+/*let heading=document.getElementById("heading");
+console.log(heading);*/
+
+
+ /*let headings=document.getElementsByClassName("heading");
+ console.log(headings);*/
+
+ /*let parahs=document.getElementsByTagName("p");
+ console.log(parahs);*/
+
+ /*let elements=document.querySelector("p");
+ console.dir(elements);
+
+ let allelements=document.querySelectorAll("p");
+ console.dir(allelements);*/
+
+ /*let firstel=document.querySelector(".heading");
+ console.dir(firstel);
+
+ let allel=document.querySelectorAll(".heading");
+ console.dir(allel);*/
+
+ /*let firstel=document.querySelector("#myButton");
+ console.dir(firstel);*/
+
+ /*let div=document.querySelector("div");
+ console.dir(div);*/
+
+
+/*let h2=document.querySelector("h2");
+console.dir(h2.innerText);
+h2.innerText=h2.innerText+" and javascript";*/
+
+let divs=document.querySelectorAll(".heading");
+console.dir(divs[0]);
