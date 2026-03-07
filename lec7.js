f688a7390dc78b8f52bc326cc0f5772f74b79ccd
digaridivyansh-create <digaridivyansh@gmail.com>
2026-03-07 19:33:11 +0530

    Add

---
+++ lec7.js
@@ -0,0 +1,43 @@
+/*let div=document.querySelector("div");
+console.log(div);
+
+let id=div.getAttribute("id");
+console.log(id);
+
+let para=document.querySelector("p");
+console.log(para.getAttribute("class"));*/
+
+/*let div=document.querySelector("div");
+div.style.backgroundColor="blue";
+div.style.fontSize="20px";
+div.style.color="white";
+div.style.width="300px";
+div.style.height="300px";
+div.style.lineHeight="300px";
+div.style.textAlign="center";
+div.innerText="this is a div";*/
+
+/*let newbtn=document.createElement("button");
+newbtn.innerText="click me";
+console.log(newbtn);
+
+let div=document.querySelector("div");
+div.append(newbtn);
+
+let newheading=document.createElement("h1");
+newheading.innerText="this is a heading";
+document.querySelector("body").prepend(newheading);
+
+let para=document.querySelector("p");
+para.remove();*/
+
+let newbtn=document.createElement("button");
+newbtn.innerText="click me";
+newbtn.style.backgroundColor="red";
+newbtn.style.color="white";
+document.querySelector("body").prepend(newbtn);
+
+let para=document.querySelector("p");
+
+
+
